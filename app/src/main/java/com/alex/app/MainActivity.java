ada73436c57df435276f5e6d8b54680c2f99b930
ChenAlex <alex.linchen@gmail.com>
2017-11-22 16:12:07 +0800

    init

--- app/src/main/java/com/alex/app/MainActivity.java
+++ app/src/main/java/com/alex/app/MainActivity.java
@@ -2,15 +2,10 @@ package com.alex.app;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.alex.widget.CirculatoryFragmentPagerAdapter;
